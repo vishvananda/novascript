c25b26979f14a7cfad7181dbc807f1ae760df029
Vishvananda Ishaya <vishvananda@gmail.com>
2010-10-21 12:54:42 -0700

    git rid of venv, update to new code, kill redis

--- nova.sh
+++ nova.sh
@@ -17,8 +17,6 @@ if [ ! -n "$HOST_IP" ]; then
     #             you should explicitly set HOST_IP in your environment
     HOST_IP=`ifconfig  | grep -m 1 'inet addr:'| cut -d: -f2 | awk '{print $1}'`
 fi
-USE_PPA=1
-USE_VENV=0
 TEST=0
 USE_MYSQL=0
 MYSQL_PASS=nova
@@ -32,17 +30,9 @@ else
 fi
 
 if [ "$USE_LDAP" == 1 ]; then
-    AUTH=LdapDriver
+    AUTH=ldapdriver.LdapDriver
 else
-    REDIS=redis-2.0.0-rc4
-    REDIS_DIR=$DIR/$REDIS
-    AUTH=FakeLdapDriver
-fi
-
-if [ "$USE_VENV" == 1 ]; then
-    VENV="$NOVA_DIR/tools/with_venv.sh "
-else
-    VENV=""
+    AUTH=dbdriver.DbDriver
 fi
 
 mkdir -p /etc/nova
@@ -54,7 +44,7 @@ cat >/etc/nova/nova-manage.conf << NOVA_CONF_EOF
 --cc_host=$HOST_IP
 --routing_source_ip=$HOST_IP
 --sql_connection=$SQL_CONN
---auth_driver=nova.auth.ldapdriver.$AUTH
+--auth_driver=nova.auth.$AUTH
 --libvirt_type=$LIBVIRT_TYPE
 NOVA_CONF_EOF
 
@@ -65,25 +55,22 @@ if [ "$CMD" == "branch" ]; then
     cd $NOVA_DIR
     mkdir -p $NOVA_DIR/instances
     mkdir -p $NOVA_DIR/networks
-    if [ "$USE_VENV" == 1 ]; then
-        sudo apt-get build-dep -y python-m2crypto
-        sudo easy_install virtualenv
-        python $NOVA_DIR/tools/install_venv.py
-        # libvirt isn't auto installed
-        cp /usr/lib/python2.6/dist-packages/*libvirt* $NOVA_DIR/.nova-venv/lib/python2.6/site-packages/
-        # libxml2 insn't auto installed
-        cp /usr/lib/pymodules/python2.6/*libxml2* $NOVA_DIR/.nova-venv/lib/python2.6/site-packages/
-    fi
 fi
 
 # You should only have to run this once
 if [ "$CMD" == "install" ]; then
-    if [ "$USE_PPA" == 1 ]; then
-        sudo apt-get install -y python-software-properties
-        sudo add-apt-repository ppa:nova-core/ppa
-        sudo apt-get update
-        sudo apt-get install -y user-mode-linux
-    fi
+    sudo apt-get install -y python-software-properties
+    sudo add-apt-repository ppa:nova-core/ppa
+    sudo apt-get update
+    sudo apt-get install -y dnsmasq vblade-persist kpartx kvm gawk iptables ebtables
+    sudo apt-get install -y user-mode-linux kvm libvirt-bin
+    sudo apt-get install -y screen aoetools euca2ools vlan curl rabbitmq-server
+    sudo modprobe aoe
+    sudo modprobe kvm
+    sudo /etc/init.d/libvirt-bin restart
+    sudo apt-get install -y python-twisted python-sqlalchemy python-mox python-greenlet python-carrot
+    sudo apt-get install -y python-daemon python-eventlet python-gflags python-tornado python-ipy
+    sudo apt-get install -y python-libvirt python-libxml2 python-routes
     if [ "$USE_MYSQL" == 1 ]; then
         cat <<MYSQL_PRESEED | debconf-set-selections
 mysql-server-5.1 mysql-server/root_password password $MYSQL_PASS
@@ -91,25 +78,6 @@ mysql-server-5.1 mysql-server/root_password_again password $MYSQL_PASS
 mysql-server-5.1 mysql-server/start_on_boot boolean true
 MYSQL_PRESEED
         apt-get install -y mysql-server python-mysqldb
-    fi
-    sudo apt-get install -y screen aoetools euca2ools vlan curl rabbitmq-server
-    sudo apt-get install -y dnsmasq vblade-persist kpartx kvm libvirt-bin
-    sudo modprobe aoe
-    sudo modprobe kvm
-    sudo apt-get install -y python-libvirt python-libxml2
-    if [ "$USE_VENV" == 0 ]; then
-        sudo apt-get build-dep -y python-m2crypto
-        sudo easy_install pip
-        sudo pip install -r $NOVA_DIR/tools/pip-requires
-        sudo pip install  "http://nova.openstack.org/Twisted-10.0.0Nova.tar.gz"
-    fi
-    if [ "$USE_LDAP" == 0 ]; then
-        rm -rf $REDIS
-        curl http://redis.googlecode.com/files/$REDIS.tar.gz -fo $REDIS.tar.gz
-        tar xvfz $REDIS.tar.gz
-        cd $REDIS
-        make
-        cd $DIR
     fi
     wget http://c2477062.cdn.cloudfiles.rackspacecloud.com/images.tgz
     tar -C $DIR -zxf images.tgz
@@ -134,9 +102,6 @@ if [ "$CMD" == "run" ]; then
     fi
     if [ "$USE_LDAP" == 1 ]; then
         sudo $NOVA_DIR/nova/auth/slap.sh
-    else
-        rm dump.rdb
-        screen_it redis "$DIR/$REDIS/redis-server"
     fi
     rm -rf $NOVA_DIR/instances
     mkdir -p $NOVA_DIR/instances
@@ -149,27 +114,27 @@ if [ "$CMD" == "run" ]; then
 
     if [ "$TEST" == 1 ]; then
         cd $NOVA_DIR
-        $VENVpython $NOVA_DIR/run_tests.py
+        python $NOVA_DIR/run_tests.py
         cd $DIR
     fi
 
     # create an admin user called 'admin'
-    $VENV$NOVA_DIR/bin/nova-manage user admin admin admin admin
+    $NOVA_DIR/bin/nova-manage user admin admin admin admin
     # create a project called 'admin' with project manager of 'admin'
-    $VENV$NOVA_DIR/bin/nova-manage project create admin admin
+    $NOVA_DIR/bin/nova-manage project create admin admin
     # export environment variables for project 'admin' and user 'admin'
-    $VENV$NOVA_DIR/bin/nova-manage project environment admin admin $NOVA_DIR/novarc
+    $NOVA_DIR/bin/nova-manage project environment admin admin $NOVA_DIR/novarc
     # create 3 small networks
-    $VENV$NOVA_DIR/bin/nova-manage network create 10.0.0.0/8 3 16
+    $NOVA_DIR/bin/nova-manage network create 10.0.0.0/8 3 16
 
     # nova api crashes if we start it with a regular screen command,
     # so send the start command by forcing text into the window.
-    screen_it api "$VENV$NOVA_DIR/bin/nova-api --flagfile=/etc/nova/nova-manage.conf"
-    screen_it objectstore "$VENV$NOVA_DIR/bin/nova-objectstore --flagfile=/etc/nova/nova-manage.conf"
-    screen_it compute "$VENV$NOVA_DIR/bin/nova-compute --flagfile=/etc/nova/nova-manage.conf"
-    screen_it network "$VENV$NOVA_DIR/bin/nova-network --flagfile=/etc/nova/nova-manage.conf"
-    screen_it scheduler "$VENV$NOVA_DIR/bin/nova-scheduler --flagfile=/etc/nova/nova-manage.conf"
-    screen_it volume "$VENV$NOVA_DIR/bin/nova-volume --flagfile=/etc/nova/nova-manage.conf"
+    screen_it api "$NOVA_DIR/bin/nova-api --flagfile=/etc/nova/nova-manage.conf"
+    screen_it objectstore "$NOVA_DIR/bin/nova-objectstore --flagfile=/etc/nova/nova-manage.conf"
+    screen_it compute "$NOVA_DIR/bin/nova-compute --flagfile=/etc/nova/nova-manage.conf"
+    screen_it network "$NOVA_DIR/bin/nova-network --flagfile=/etc/nova/nova-manage.conf"
+    screen_it scheduler "$NOVA_DIR/bin/nova-scheduler --flagfile=/etc/nova/nova-manage.conf"
+    screen_it volume "$NOVA_DIR/bin/nova-volume --flagfile=/etc/nova/nova-manage.conf"
     screen_it test ". $NOVA_DIR/novarc"
     screen -x
 fi
@@ -181,10 +146,6 @@ if [ "$CMD" == "run" ] || [ "$CMD" == "terminate" ]; then
 fi
 
 if [ "$CMD" == "run" ] || [ "$CMD" == "clean" ]; then
-    if [ "$USE_LDAP" == 0 ]; then
-        # redis simply disconnects on screen kill so force it to die
-        killall redis-server
-    fi
     screen -S nova -X quit
     rm *.pid*
     $NOVA_DIR/tools/setup_iptables.sh clear
