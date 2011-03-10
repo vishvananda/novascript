0a32d1df0eedb3550210734254c2606e622d32e8
Vishvananda Ishaya <vishvananda@gmail.com>
2011-03-09 21:28:26 -0800

    add bin to the path so nova-manage commands are easier

--- nova.sh
+++ nova.sh
@@ -191,6 +191,7 @@ NOVA_CONF_EOF
     $NOVA_DIR/bin/nova-manage project zipfile admin admin $NOVA_DIR/nova.zip
     unzip -o $NOVA_DIR/nova.zip -d $NOVA_DIR/
 
+    screen_it test "export PATH=$NOVA_DIR/bin:$PATH"
     screen_it test ". $NOVA_DIR/novarc"
     if [ "$CMD" != "run_detached" ]; then
       screen -S nova -x
